f2154991e8017ca8a5f2925e6ab351b128929c83
kalikt <kalikt2004@gmail.com>
2024-12-07 18:24:40 +0200

    Changes for review create and details view

---
+++ TastyOrders.Web/wwwroot/js/reviewStar.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const stars = document.querySelectorAll('.star');
+    const ratingInput = document.getElementById('ratingInput');
+
+    stars.forEach(star => {
+        star.addEventListener('click', function () {
+            const value = this.dataset.value;
+            ratingInput.value = value;
+
+            stars.forEach(s => {
+                s.classList.remove('active');
+                if (s.dataset.value <= value) {
+                    s.classList.add('active');
+                }
+            });
+        });
+    });
+});
