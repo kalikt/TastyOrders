008be7ec78b5a1b3c8d683be11f3644ce12882b0
kalikt <kalikt2004@gmail.com>
2024-12-10 20:00:49 +0200

    Added back button to review details view

---
+++ TastyOrders.Web.ViewModels/Review/ReviewListViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TastyOrders.Web.ViewModels.Review
+{
+    public class ReviewListViewModel
+    {
+        public int RestaurantId { get; set; }
+        public string RestaurantName { get; set; } = string.Empty;
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+
+        public string Location {  get; set; } = string.Empty;
+    }
+}
